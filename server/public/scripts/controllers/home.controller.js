b5103762deecbbd5fbe2bd78f805da219d7de6f1
Andrew Johnson <ajj@byu.net>
2018-10-27 15:25:13 -0500

    Poll a Particle variable

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('HomeController', ['HomeService', '$interval', function (HomeService, $interval) {
+myApp.controller('HomeController', ['HomeService', '$interval', '$http', function (HomeService, $interval, $http) {
     let self = this;
     
     self.andrew = 'hi';
@@ -8,36 +8,29 @@ myApp.controller('HomeController', ['HomeService', '$interval', function (HomeSe
 
 
 
-      //10 seconds delay
-    $interval( function(){
-      self.test1 = "Hello World!";
-    }, 5000 );
-
     //time
     self.time = 0;
     
     //timer callback
-    var timer = function() {
-      $interval(this.pollStream, 1000);
-    }
+    // var timer = function() {
+    //   $interval(self.pollStream, 1000);
+    // }
     
-    //run!!
-    let i = 0;
-    $interval(function() { self.stream.push(i++); }, 100);  
-
-
     this.pollStream = function() {
-      self.stream.push('1');
-      // return $http.get('https://api.particle.io/v1/devices/1c003b000c47363339343638/analogvalue?access_token=05700d7c927634766baf2972975055959d379d3c')
-      //   .then( function(response) {
-      //     this.stream.push(response);
-      //     console.log('response');
-      //     return response.data;
-      //   })
-      //   .catch( function(error) {
-      //     console.log(error);
-      //     return 'error';
-      //   });
+      
+      $http.get('https://api.particle.io/v1/devices/1c003b000c47363339343638/analogvalue?access_token=05700d7c927634766baf2972975055959d379d3c')
+        .then( function(response) {
+
+          self.stream.push(response);
+          console.log('response');
+          return response.data;
+        })
+        .catch( function(error) {
+          console.log(error);
+          return 'error';
+        });
+        
     }
+    $interval(function() { self.pollStream(); }, 1000);
    
 }]);
