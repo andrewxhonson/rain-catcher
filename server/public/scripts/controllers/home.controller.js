bce2b89356ba486d6663432a2c717e8d238073c4
Andrew Johnson <ajj@byu.net>
2018-10-27 15:51:11 -0500

    Buttons to drain

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -6,7 +6,18 @@ myApp.controller('HomeController', ['HomeService', '$interval', '$http', functio
     self.stream = [];
 
 
+    self.drainMode = false;
+    self.drainModeLastTried = false;
 
+    self.drain = function(action) {
+      if (action) { // start
+        self.drainModeLastTried = true;
+
+      } else {
+        self.drainModeLastTried = false;
+      }
+
+    };
 
     //time
     self.time = 0;
@@ -21,7 +32,7 @@ myApp.controller('HomeController', ['HomeService', '$interval', '$http', functio
       $http.get('https://api.particle.io/v1/devices/1c003b000c47363339343638/analogvalue?access_token=05700d7c927634766baf2972975055959d379d3c')
         .then( function(response) {
 
-          self.stream.push(response);
+          self.stream.unshift(response);
           console.log('response');
           return response.data;
         })
